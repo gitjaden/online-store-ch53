8e166ee02255e5e8e0bf4394e567d1c26e6fc619
gitjaden <jadensherpa9@gmail.com>
2024-12-21 15:11:57 -0800

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import './styles/navbar.css'
+
+function Navbar() {
+    return (
+        <div>
+            <nav>
+                <div class="navbar-menu">
+                    <a href="">Log In</a>
+                    <a href="">Search</a>
+                    <a href="">Subscribe</a>
+                    <a href="">Menu</a>
+                    </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
+
